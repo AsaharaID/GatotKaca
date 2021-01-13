c5d72bb4bd7260328ff964a51289dd3f8995b74c
AsaharaID <muhamadsyahr45@gmail.com>
2021-01-13 18:10:38 +0700

    Initial commit

---
+++ berita/layout/nav.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="./css/head-style.css">
+<body>
+<nav class="navbar navbar-expand-sm bg-light navbar-custom sticky-top">
+    <a class="navbar-brand" href="https://www.ea.com/en-gb/games/apex-legends" target="_blank">
+        <img src="../assets/GatotKaca.png" class="float-left img-fluid" alt="APEX-LOGO-4" width="60%" onclick="window.open('https://www.ea.com/en-gb/games/apex-legends', '_blank');">
+    </a>
+    <ul class="navbar-nav navbar-center">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Berita</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Hoax</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Personal</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Bantuan</a>
+        </li>
+    </ul>
+    <ul class="navbar-nav navbar-right">
+        <li><a class="nav-Daftar" href="#">Daftar</a></li>
+        <li><a class="nav-Masuk"  href="#">Masuk</a></li>
+    </ul>
+</nav>
+</body>
