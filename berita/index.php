7f29ab2704bb64bf49e60c39e9336000cdfa14f5
AsaharaID <muhamadsyahr45@gmail.com>
2021-01-14 21:13:03 +0700

    initial commit

--- berita/index.php
+++ berita/index.php
@@ -10,6 +10,8 @@
     <footer>
         <?php
         include_once "layout/nav.php";
+        include_once "layout/berita-hari-ini.php";
+        include_once "layout/trend.php";
         include_once "layout/footer.php";
         ?>
     </footer>
